5dd11fb59bbe393a132129aa7067850dea96efa7
AAKASH KUMAR <162182656+akprajapat5501@users.noreply.github.com>
2026-02-19 15:57:48 +0530

    Change API URL to production
    Updated the API URL to the production endpoint.

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -5,6 +5,7 @@ import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 
+export const url = 'https://food-delivery-app-116h.onrender.com'
 export const assets ={
     logo,
     add_icon,
@@ -14,4 +15,3 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'http://localhost:4000'
